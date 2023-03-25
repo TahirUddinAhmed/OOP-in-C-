d310c9953fb0181715c2d096a07a96b641a0821d
Tahir Uddin Ahmed <95581667+TahirUddinAhmed@users.noreply.github.com>
2023-03-25 08:52:16 +0530

    Update hello.cpp
    add a new line

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main() {
     cout<<"Hi there!welcome to world of C++"<<endl;
     cout<<"ADD a new line"<<endl;
+    
+    int a = 5, b = 5;
+    cout<<"a + b is : "<<a + b<<endl;
     return 0;
 }
