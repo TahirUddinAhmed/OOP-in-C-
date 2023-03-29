227102e21d07b73efc932b813c16fecdd6aa7072
Tahir Ahmed <01tahirahmed@gmail.com>
2023-03-29 20:47:46 +0530

    add arrays of objects file

--- OOP/array of objects/arrOfObj1.cpp
+++ OOP/array of objects/arrOfObj1.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
+class Employee {
+  int emp_id;
+  char name[30];
+  
+  public:
+   void getData() {
+     cout<<"Enter the employee id\n: ";
+     cin>>emp_id;
+     cout<<"Enter the employee name\n: ";
+     cin>>name;
+   }
+   void putData() {
+     cout<<"Id: "<<emp_id;
+     cout<<" Name: "<<name<<endl;
+   }
+};
+
 int main() {
- 
+ // array of objects
+ Employee manager[60];
+ int n;
+ cout<<"Enter how many employee are there?\n: ";
+ cin>>n;
+
+// take input 
+ for(int i = 0; i < n; i++) {
+    manager[i].getData();
+ }
+
+// display manager's details
+cout<<"\nAll manager's details:- "<<endl;
+for(int i = 0; i < n; i++) {
+   manager[i].putData();
+}
 
  return 0;
 }
