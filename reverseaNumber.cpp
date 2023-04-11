d9e5aee51dd5db4a1f720cb4955058c1ba04970d
Tahir Ahmed <01tahirahmed@gmail.com>
2023-04-11 11:33:10 +0530

    improve the reverseNumber.cpp file

--- reverseaNumber.cpp
+++ reverseaNumber.cpp
@@ -2,18 +2,45 @@
 using namespace std;
 
 int main() {
-    int num, rem;
+    int num, result;
     cout<<"Enter a positive number: ";
     cin>>num;
 
 
     cout<<"Reverse number= ";
     while(num != 0) {
-        rem = num % 10;
-        cout<<rem;
-        num = num / 10;
+        result = num % 10;
+        cout<<result;
+        num = num / 10; 
     }
 
     cout<<endl;
     return 0;
 }
+
+
+/*
+let the number be 123
+
+loop: 
+
+round1: 
+  result = 123 % 10   = 3       as the number 123 is integer value
+  print-  3
+  num = 123 / 10      = 12  
+
+round2: 
+  result = 12 % 10 = 2
+  print - 32
+  num = 12 / 10    = 1 
+
+round3: 
+  result = 1 % 10 = 1
+  print  -   321 
+  num = 1 / 10    = 0
+
+  stop the loop 
+
+  so the result is = 321
+
+*/
